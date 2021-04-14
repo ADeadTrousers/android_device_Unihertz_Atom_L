6dbd8d893af217915652bf3ab108d72307bda572
Matthias Leitl <a.dead.trousers@gmail.com>
2021-04-14 19:19:24 +0200

    Splitting repositories for EEA / TEE support

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,14 @@
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+-include device/Unihertz/Atom_LXL/BoardConfigCommon.mk
+
+DEVICE_MODEL_PATH := device/Unihertz/Atom_L
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_MODEL_PATH)/bluetooth/include
+
+# Properties
+TARGET_PRODUCT_PROP += $(DEVICE_MODEL_PATH)/product.prop
